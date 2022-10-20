3037c0f61ad3a4631a080df0b099d6cc21207334
kfq77 <1369811973@qq.com>
2022-10-21 02:50:12 +1100

    My module

---
+++ app/src/main/java/com/example/comp5216_petloversapp/MainActivity3.java
@@ -0,0 +1,25 @@
+package com.example.comp5216_petloversapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity3 extends AppCompatActivity {
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        imageView = findViewById(R.id.iv_modify);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity3.this,MainActivity2.class));
+            }
+        });
+    }
+}
